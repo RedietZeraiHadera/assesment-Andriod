0b3cb09af810cc70a697fa6bb9f38327f6a03b44
Wawasisaru <nsaruwawasi@gmail.com>
2023-06-20 10:21:06 +0300

    commit

--- app/src/main/java/com/example/billmanagement/MainActivity.kt
+++ app/src/main/java/com/example/billmanagement/MainActivity.kt
@@ -8,34 +8,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.billmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
     }
 
     override fun onResume() {
         super.onResume()
-        displayPerson()
-
-        binding.rvBills.setOnClickListener {
-            val intent = Intent(this, BillsList::class.java)
-            startActivity(intent)
+        binding.btnSignup.setOnClickListener {
+            startActivity(Intent(this,Registration::class.java)
         }
-
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this,Login::class.java)
+        }
     }
-    fun displayPerson(){
-        val person1 = BillsList("RedietZerai","0927444862","redietzerai@gmail.com","qw2020")
-        val person2 = BillsList("ShalomZerai","0927444862","redietzerai@gmail.com","qw2020")
 
-        val personLists = mutableListOf(person1,person2)
-
-        var billList= listOf<BillsList>(person1,person2)
-        binding.rvBills.layoutManager=LinearLayoutManager(this)
-        var adapter= (billList)
-
+    fun validate(){
+        val name = binding.etName.text.toString()
+        val phone = binding.etPhone.text.toString()
+        val password = binding.etPassword.toString()
     }
+}
+    }
+
 
 }
